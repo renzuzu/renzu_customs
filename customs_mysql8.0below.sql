1393458a546c94a2e2f02d6473f3ead7537a2391
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-09-11 23:14:02 +0800

    mysql ver

---
+++ customs_mysql8.0below.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `renzu_customs` (
+	`shop` VARCHAR(64) NULL COLLATE 'utf8mb4_general_ci',
+	`inventory` LONGTEXT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`shop`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
